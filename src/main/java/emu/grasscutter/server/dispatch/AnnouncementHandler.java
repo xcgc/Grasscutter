2aa21fd6e6c376e35d9102410dd5b39ea216ffba
Hotaru <105128850+HotaruYS@users.noreply.github.com>
2022-05-14 22:04:35 +0200

    Properly handle static assets for announcements

--- src/main/java/emu/grasscutter/server/dispatch/AnnouncementHandler.java
+++ src/main/java/emu/grasscutter/server/dispatch/AnnouncementHandler.java
@@ -17,16 +17,28 @@ import static emu.grasscutter.Configuration.*;
 public final class AnnouncementHandler implements HttpContextHandler {
     @Override
     public void handle(Request request, Response response) throws IOException {
+        String data = "";
         if (Objects.equals(request.baseUrl(), "/common/hk4e_global/announcement/api/getAnnContent")) {
-            String data = readToString(Paths.get(DATA("GameAnnouncement.json")));
-
-            response.send("{\"retcode\":0,\"message\":\"OK\",\"data\":" + data + "}");
+            data = readToString(Paths.get(DATA("GameAnnouncement.json")));
         } else if (Objects.equals(request.baseUrl(), "/common/hk4e_global/announcement/api/getAnnList")) {
-            String data = readToString(Paths.get(DATA("GameAnnouncementList.json")))
-                .replace("System.currentTimeMillis()", String.valueOf(System.currentTimeMillis()));
-
-            response.send("{\"retcode\":0,\"message\":\"OK\",\"data\": " + data + "}");
+            data = readToString(Paths.get(DATA("GameAnnouncementList.json")));
+        } else {
+            response.send("{\"retcode\":404,\"message\":\"Unknown request path\"}");
         }
+
+        if (data.isEmpty()) {
+            response.send("{\"retcode\":500,\"message\":\"Unable to fetch requsted content\"}");
+            return;
+        }
+
+        String dispatchDomain = "http" + (DISPATCH_ENCRYPTION.useInRouting ? "s" : "") + "://"
+            + lr(DISPATCH_INFO.accessAddress, DISPATCH_INFO.bindAddress) + ":"
+            + lr(DISPATCH_INFO.accessPort, DISPATCH_INFO.bindPort);
+
+        data = data
+            .replace("{{DISPATCH_PUBLIC}}", dispatchDomain)
+            .replace("{{SYSTEM_TIME}}", String.valueOf(System.currentTimeMillis()));
+        response.send("{\"retcode\":0,\"message\":\"OK\",\"data\": " + data + "}");
     }
 
     private static String readToString(Path path) {
@@ -35,7 +47,7 @@ public final class AnnouncementHandler implements HttpContextHandler {
         try {
             content = Files.readString(path, StandardCharsets.UTF_8);
         } catch (IOException ignored) {
-            Grasscutter.getLogger().warn("Unable to open file " + path.toAbsolutePath());
+            Grasscutter.getLogger().warn("File does not exist: " + path);
         }
         
         return content;
